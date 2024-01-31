c89893c1f7ee039295692a99edd0164d36111bc6
Laxmikant <laxmikant.baheti.s@gmail.com>
2024-01-31 08:05:46 +0100

    metadata

--- src/setup.py
+++ src/setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 
-setup(name='mypackage',
+setup(name='mlpro_int_optuna',
 version='1.0.0',
-description='MLPro: Integration River',
+description='MLPro: Integration Optuna',
 author='MLPro Team',
 author_mail='mlpro@listen.fh-swf.de',
 license='Apache Software License (http://www.apache.org/licenses/LICENSE-2.0)',
-packages=['mypackage'],
+packages=['mlpro_int_optuna'],
 
 # Package dependencies for full installation
 extras_require={
